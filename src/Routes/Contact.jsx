2c6fe64b919a538d3342f46987ecab0f8de3871b
cerebrium <nicholas.m.shankland@gmail.com>
2019-12-27 10:12:40 -0800

    got the submit button working

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,22 +1,50 @@
 import React from 'react'; 
+import '../styles/App.css'
 
-class Contact extends React.Component {
-    render () {
-        return (
-            <div className='mainContainer'>
-                <div className='rowCentering'>
-                    <div className='contentCentering pictureBackground'>
-                        <h1>Contact</h1>
-                        <textarea name="contactMe" id="" cols="30" rows="100"></textarea>
-                        <br />
-                        <form action="/contact" method='GET'>
-                            <input type="submit" value='Send Us An Email' className='submitButton'/>
-                        </form>
-                    </div>
-                </div> 
-            </div>
-        )
+    export default class Contact extends React.Component {
+
+        constructor(props) {
+            super(props);
+            this.submitForm = this.submitForm.bind(this);
+            this.state = {
+            status: ""
+            };
+        }
+        
+        submitForm = (ev) => {
+            ev.preventDefault();
+            const form = ev.target;
+            const data = new FormData(form);
+            const xhr = new XMLHttpRequest();
+            xhr.open(form.method, form.action);
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                this.setState({ status: "SUCCESS" });
+            } else {
+                this.setState({ status: "ERROR" });
+            }
+            };
+            xhr.send(data);
+        }
+
+        render() {
+            const { status } = this.state;
+            return (
+                <div className='contactPageMain'>
+                    <h1>Contact Us!</h1>
+                    <form onSubmit={this.submitForm} action='https://formspree.io/meqelkae' method="POST">
+                        <label >Name:</label>{'  '}
+                        <input type="text" name="name"/><br/>
+                        <label>Email:</label>{'  '}
+                        <input type="email" name="email" /><br/>
+                        <textarea type="textarea" rows='15' cols='16' name="message" className='myTextArea'></textarea><br/>
+                        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+                        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+                    </form>
+                </div>
+            );
+        }
     }
-}
-
-export default Contact;
